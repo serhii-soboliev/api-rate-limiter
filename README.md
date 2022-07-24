7e14d0c5c2879a33578a50beb59188d2b2819f50
sergiy.sobolev@gmail.com <sergiy.sobolev@gmail.com>
2022-07-24 11:37:02 +0300

    Fix data race with mutex

--- README.md
+++ README.md
@@ -4,12 +4,15 @@
 
 API Rate limiter service for demonstration purpose
 
+# Design
+
 # Prerequisites
 
 Install GoLang https://go.dev
 
 # How to run
-1. git clone https://github.com/serhii-soboliev/api-rate-limiter.git
+
+1. git clone <https://github.com/serhii-soboliev/api-rate-limiter.git>
 2. cd api-rate-limiter/cmd
 3. go build && go run .
 
--- pkg/fixedwindow/fixed_window_api_rate_limiter.go
+++ pkg/fixedwindow/fixed_window_api_rate_limiter.go
@@ -2,37 +2,45 @@ package fixedwindow
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 type FixedWindowUser struct {
 	UserID     string `json:"userid"`
-	Last       int64 `json:"last"`
-	Count      int `json:"count"`
-	WindowSize int64 `json:"windowsize"`
+	Last       int64  `json:"last"`
+	Count      int    `json:"count"`
+	WindowSize int64  `json:"windowsize"`
 }
 
 var windowSize int64 = 60
 
 var fixedWindowUsers = make(map[string]FixedWindowUser)
 
+var mux = sync.Mutex{}
+
 func GetResource(userId string) bool {
+
 	if v, found := fixedWindowUsers[userId]; found {
+		mux.Lock()
 		last := v.Last
 		current := time.Now().Unix()
-		if current - last > windowSize {
+		if current-last > windowSize {
 			fwu := FixedWindowUser{userId, time.Now().Unix(), 1, windowSize}
-			fixedWindowUsers[userId] = fwu	
+			fixedWindowUsers[userId] = fwu
+			mux.Unlock()
 			return true
 		} else if v.Count < 3 {
 			fmt.Println(v)
 			v.Count = v.Count + 1
 			fixedWindowUsers[userId] = v
+			mux.Unlock()
 			return true
 		} else {
 			fmt.Println(v)
+			mux.Unlock()
 			return false
-		}	
+		}
 	} else {
 		fwu := FixedWindowUser{userId, time.Now().Unix(), 1, windowSize}
 		fixedWindowUsers[userId] = fwu
